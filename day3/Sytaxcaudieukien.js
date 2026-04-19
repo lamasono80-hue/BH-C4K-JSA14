2b33fd8146a2a2b9acf4100d03e598fdec8c08b8
lamasono <lamasono80@gmail.com>
2026-04-19 17:46:12 +0700

    upadate forloop - caudieukien

--- day3/Sytaxcaudieukien.js
+++ day3/Sytaxcaudieukien.js
@@ -1,1 +1,23 @@
-console.log("liên kết thành công rồi");
+//var age = 30;
+//if (age >= 30) {
+//console.log("đủ tuổi uống rượu");
+//} else {
+//  console.log("chưa đủ tuổi uống rượu");
+//}
+//======================================
+var toan = 5;
+var ly = 4.9;
+var tienganh = 4.9;
+var diemTB = (toan + ly + tienganh) / 3;
+if (diemTB >= 9 && diemTB <= 10) {
+  console.log("xuất sắc");
+} else if (diemTB >= 8 && diemTB < 8.9) {
+  console.log("giỏi");
+} else if (diemTB >= 7 && diemTB < 7.9) {
+  console.log("trung bình");
+} else if (diemTB >= 5 && diemTB < 6.9) {
+  console.log("điểm khá");
+} else if ((diemTB) => 4 && diemTB <= 4.9) {
+  console.log("yếu");
+}
+//======================================
